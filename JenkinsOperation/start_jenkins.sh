6d85d0e5237cfadf209986f8633b8ea23fb56ba7
threck <threck@qq.com>
2021-01-28 17:30:29 +0800

    code refine: fix a bug of start_jenkins.sh

--- JenkinsOperation/start_jenkins.sh
+++ JenkinsOperation/start_jenkins.sh
@@ -4,9 +4,18 @@ LOCALDIR=$(cd $(dirname $0) && pwd && cd - &> /dev/null)
 BASEDIR=${LOCALDIR}/..
 source ${BASEDIR}/common.sh
 
+
+if [ -z "$1" ]; then
+  echo "please run as: bash $0 [ jenkins_nodename ]"
+  echo "e.g. bash $0 2k_miner_150"
+  exit 1
+fi
+
 t=$(date +%Y%m%d%H%M%S)
 ip=$(ifconfig |grep 192 |awk -F' ' '{print $2}')
 log=$0${t}.log
+nodename=$1
+
 
 # judge if there's a jenkins process
 pid=$(ps -ef |grep jenkins-agent |grep -v grep |awk -F' ' '{print $2}')
@@ -18,7 +27,7 @@ fi
 
 # start jenkins
 log_info "starting jenkins on ${ip} ..."
-nohup java -jar ${LOCALDIR}/agent.jar -jnlpUrl http://${ip}:8081/computer/miner_201/jenkins-agent.jnlp \
+nohup java -jar ${LOCALDIR}/agent.jar -jnlpUrl http://192.168.0.4:8081/computer/miner_201/jenkins-agent.jnlp \
 -secret @secret-file -workDir "/var/jenkins_home" &> ${log} &
 
 # check if jenkins start success
