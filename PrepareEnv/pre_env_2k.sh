d6450413300d7ece6efb7b3299c2622a3702ab8b
threck <threck@qq.com>
2021-02-01 14:03:17 +0800

    code refine: add cluster info

--- PrepareEnv/pre_env_2k.sh
+++ PrepareEnv/pre_env_2k.sh
@@ -14,12 +14,15 @@ fi
 
 # check cluster network
 conf_file=$1
+miner_ip=$(grep -v '^ *#' ${conf_file} |grep "miner" |awk -F' ' '{print $2}'|cut -d'=' -f2)
+worker_ip=$(grep -v '^ *#' ${conf_file} |grep "worker" |awk -F' ' '{print $2}'|cut -d'=' -f2)
+log_info "cluster miner: ${miner_ip}"
+log_info "cluster worker: ${worker_ip}"
 cluster_list=$(grep -v '^ *#' ${conf_file} |grep -E "worker|miner" |awk -F' ' '{print $2}'|cut -d'=' -f2)
 check_network_connection "${cluster_list}"
 [ $? -ne 0 ] && exit 1
 
 # 1. copy miner_pre script to miner_ip
-miner_ip=$(grep -v '^ *#' ${conf_file} |grep "miner" |awk -F' ' '{print $2}'|cut -d'=' -f2)
 run_command_remote ${miner_ip} ${M_USER} ${M_PWD} "mkdir -p ${LOCALDIR}"
 cp_to_remote ${LOCALDIR}/pre_env_2k_lotus_miner.sh ${miner_ip} ${M_USER} ${M_PWD} ${LOCALDIR}
 cp_to_remote ${conf_file} ${miner_ip} ${M_USER} ${M_PWD} ${LOCALDIR}
@@ -32,7 +35,6 @@ run_command_remote ${miner_ip} ${M_USER} ${M_PWD} "bash ${LOCALDIR}/pre_env_2k_l
 
 # 3. copy worker_pre script to worker_ip
 exit_value=0
-worker_ip=$(grep -v '^ *#' ${conf_file} |grep "worker" |awk -F' ' '{print $2}'|cut -d'=' -f2)
 for worker_ip_tmp in ${worker_ip}; do
   run_command_remote ${worker_ip_tmp} ${M_USER} ${M_PWD} "mkdir -p ${LOCALDIR}"
   cp_to_remote ${LOCALDIR}/pre_env_2k_worker.sh ${worker_ip_tmp} ${M_USER} ${M_PWD} ${LOCALDIR}
