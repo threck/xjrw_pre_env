1ccd59e1c50f06a8836f7113adcf3a1ca3dad1e9
threck <threck@qq.com>
2021-03-16 14:08:00 +0800

    code refine: modify restart_jenkins.sh; miner and workers' env var

--- JenkinsOperation/restart_jenkins.sh
+++ JenkinsOperation/restart_jenkins.sh
@@ -4,7 +4,7 @@ LOCALDIR=$(cd $(dirname $0) && pwd && cd - &> /dev/null)
 
 if [ -z "$1" ]; then
   echo "please run as: bash $0 [ jenkins_nodename ]"
-  echo "e.g. bash $0 2k_miner_150"
+  echo "e.g. bash $0 192.168.0.10"
   exit 1
 fi
 cd ${LOCALDIR}
